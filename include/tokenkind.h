463fdfd58b00f3c5361717aea1b12510a5e7b93f
Ananyo Bhattacharya <169988112+UniquePython@users.noreply.github.com>
2026-08-10 00:02:40 +0530

    Add a token kind representation

---
+++ include/tokenkind.h
@@ -0,0 +1,16 @@
+#ifndef TOKENKIND_H_
+#define TOKENKIND_H_
+
+typedef enum tokenkind_t
+{
+    TK_IDENTIFIER,
+    TK_KEYWORD,
+    TK_NUMBER,
+    TK_COLON,
+    TK_SEMICOLON,
+    TK_EOF,
+} TokenKind;
+
+const char *GetTokenKindName(TokenKind tk);
+
+#endif /* TOKENKIND_H_ */
---
+++ src/tokenkind.c
@@ -0,0 +1,28 @@
+#include "tokenkind.h"
+
+const char *GetTokenKindName(TokenKind tk)
+{
+    switch (tk)
+    {
+    case TK_IDENTIFIER:
+        return "identifier";
+
+    case TK_KEYWORD:
+        return "keyword";
+
+    case TK_NUMBER:
+        return "number";
+
+    case TK_COLON:
+        return "colon";
+
+    case TK_SEMICOLON:
+        return "semicolon";
+
+    case TK_EOF:
+        return "end of file";
+
+    default:
+        return "unknown";
+    }
+}
