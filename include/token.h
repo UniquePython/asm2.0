20d724d9a653862df8fb75d9180e881bd5d74f86
Ananyo Bhattacharya <169988112+UniquePython@users.noreply.github.com>
2026-08-10 09:53:17 +0530

    Add keyword, number and identifier fields to token

--- include/token.h
+++ include/token.h
@@ -1,13 +1,22 @@
 #ifndef TOKEN_H_
 #define TOKEN_H_
 
+#include "types.h"
 #include "span.h"
+#include "keyword.h"
 #include "tokenkind.h"
 
 typedef struct token_t
 {
     TokenKind tk;
     Span span;
+
+    union token_as_t
+    {
+        Keyword keyword;
+        u64 number;
+        const char *identifier;
+    } as;
 } Token;
 
 #endif /* TOKEN_H_ */
